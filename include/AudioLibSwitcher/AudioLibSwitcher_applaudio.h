546a91c734a1525aaa233b4ded19bc07640e1127
Rasmus Anthin <scratty@gmail.com>
2025-09-15 13:58:09 +0200

    Added new header AudioLibSwitcher_applaudio. Stub so far.

---
+++ include/AudioLibSwitcher/AudioLibSwitcher_applaudio.h
@@ -0,0 +1,16 @@
+//
+//  AudioLibSwitcher_applaudio.h
+//  AudioLibSwitcher_applaudio
+//
+//  Created by Rasmus Anthin on 2025-09-15.
+//
+
+#pragma once
+#include "../../AudioLibSwitcher/include/AudioLibSwitcher/IAudioLibSwitcher.h"
+
+namespace audio
+{
+
+
+
+}
